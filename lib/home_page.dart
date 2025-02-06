ee6e7971aad04c786009d337364994dd15e53501
Richard Max <82667541+dingsboms@users.noreply.github.com>
2025-02-06 08:35:14 +0100

    feat: Add notes to tiles

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -129,6 +129,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       : getColorForTemperature(temperature.toDouble());
 
                   final isKnitted = data['is_knitted'] || false;
+                  final String knittingNote = data['knitting_note'] ?? "";
                   final docId = doc.id;
 
                   // Check if it's the first day of a month
@@ -140,6 +141,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     backgroundColor: backgroundColor,
                     isNewMonth: isNewMonth,
                     isKnitted: isKnitted,
+                    knittingNote: knittingNote,
                     docId: docId
                   );
                 }).toList();
@@ -216,7 +218,7 @@ class _MyHomePageState extends State<MyHomePage> {
                                         ),
                                       ),
                                       child: CupertinoListTile(
-                                        leadingSize: 75,
+                                        leadingSize: 90,
                                         title: Text(
                                           '${item.temperature.round()} °C',
                                         ),
@@ -226,6 +228,138 @@ class _MyHomePageState extends State<MyHomePage> {
                                         ),
                                         subtitle: Text(
                                           '${item.localDate.hour.toString().padLeft(2, "0")}:${item.localDate.minute.toString().padLeft(2, "0")}',
+                                        ),
+                                        additionalInfo: IconButton(
+                                          onPressed: () {
+                                            showCupertinoModalPopup<void>(
+                                              context: context,
+                                              builder: (BuildContext context) {
+                                                // Text editing controllers to manage input
+                                                final TextEditingController
+                                                    noteController =
+                                                    TextEditingController();
+
+                                                noteController.text =
+                                                    item.knittingNote;
+
+                                                return Container(
+                                                  height:
+                                                      400, // Increased height to accommodate form
+                                                  padding:
+                                                      const EdgeInsets.all(16),
+                                                  decoration:
+                                                      const BoxDecoration(
+                                                    color: CupertinoColors
+                                                        .systemBackground,
+                                                    borderRadius:
+                                                        BorderRadius.only(
+                                                      topLeft:
+                                                          Radius.circular(12),
+                                                      topRight:
+                                                          Radius.circular(12),
+                                                    ),
+                                                  ),
+                                                  child: Column(
+                                                    mainAxisSize:
+                                                        MainAxisSize.min,
+                                                    children: <Widget>[
+                                                      const Text(
+                                                        'Write a note',
+                                                        style: TextStyle(
+                                                            fontSize: 20,
+                                                            fontWeight:
+                                                                FontWeight
+                                                                    .bold),
+                                                      ),
+                                                      const SizedBox(
+                                                          height: 20),
+                                                      CupertinoTextField(
+                                                        controller:
+                                                            noteController,
+                                                        padding:
+                                                            const EdgeInsets
+                                                                .all(12),
+                                                        minLines:
+                                                            5, // This will make it 5 lines tall minimum
+                                                        maxLines:
+                                                            10, // This allows it to grow up to 10 lines
+                                                        style: const TextStyle(
+                                                          color: CupertinoColors
+                                                              .black, // This fixes the text color
+                                                        ),
+                                                        decoration:
+                                                            BoxDecoration(
+                                                          border: Border.all(
+                                                              color: CupertinoColors
+                                                                  .systemGrey),
+                                                          borderRadius:
+                                                              BorderRadius
+                                                                  .circular(8),
+                                                        ),
+                                                      ),
+                                                      const SizedBox(
+                                                          height: 20),
+                                                      Row(
+                                                        mainAxisAlignment:
+                                                            MainAxisAlignment
+                                                                .spaceEvenly,
+                                                        children: [
+                                                          CupertinoButton(
+                                                            child: const Text(
+                                                                'Cancel'),
+                                                            onPressed: () =>
+                                                                Navigator.pop(
+                                                                    context),
+                                                          ),
+                                                          CupertinoButton
+                                                              .filled(
+                                                            child: const Text(
+                                                                'Submit'),
+                                                            onPressed: () {
+                                                              FirebaseFirestore
+                                                                  .instance
+                                                                  .collection(
+                                                                      "days")
+                                                                  .doc(item
+                                                                      .docId)
+                                                                  .set(
+                                                                      {
+                                                                    "knitting_note":
+                                                                        noteController
+                                                                            .text
+                                                                  },
+                                                                      SetOptions(
+                                                                          merge:
+                                                                              true));
+
+                                                              Navigator.pop(
+                                                                  context);
+                                                            },
+                                                          ),
+                                                        ],
+                                                      ),
+                                                    ],
+                                                  ),
+                                                );
+                                              },
+                                            );
+                                          },
+                                          icon: item.knittingNote == ""
+                                              ? Icon(Icons.note_add)
+                                              : Icon(Icons.note),
+                                          style: ButtonStyle(
+                                            backgroundColor:
+                                                WidgetStateProperty.all<Color>(
+                                                    Colors.white),
+                                            foregroundColor:
+                                                WidgetStateProperty.all<Color>(
+                                                    Colors.blue),
+                                            padding: WidgetStateProperty.all<
+                                                EdgeInsets>(
+                                              const EdgeInsets.symmetric(
+                                                  horizontal: 16, vertical: 8),
+                                            ),
+                                          ),
                                         ),
                                         trailing: CupertinoCheckbox(
                                           inactiveColor: Colors.white,
