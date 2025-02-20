d8143355f87af0521a7f8700e8d7ecddf4018c61
Richard Max <82667541+dingsboms@users.noreply.github.com>
2025-02-20 18:01:31 +0100

    revert to CupertinoTextField

--- lib/components/generic/small_text_field.dart
+++ lib/components/generic/small_text_field.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
+final BoxDecoration textFieldDecoration = BoxDecoration(
+  color: CupertinoColors.white,
+  border: Border.all(color: CupertinoColors.black.withOpacity(0.5), width: 1.0),
+  borderRadius: BorderRadius.circular(5.0),
+);
 
 const TextStyle textStyle = TextStyle(color: CupertinoColors.black);
 
 class SmallTextField extends StatelessWidget {
-  const SmallTextField({
-    super.key,
-    required this.controller,
-    this.formatters,
-    this.keyboardType,
-  });
-
+  const SmallTextField(
+      {super.key,
+      required this.controller,
+      this.formatters,
+      this.keyboardType});
   final TextEditingController controller;
   final List<TextInputFormatter>? formatters;
   final TextInputType? keyboardType;
@@ -20,37 +23,11 @@ class SmallTextField extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       width: 80,
-      child: TextField(
+      child: CupertinoTextField(
         controller: controller,
         style: textStyle,
         keyboardType: keyboardType,
-        decoration: InputDecoration(
-          filled: true,
-          fillColor: CupertinoColors.white,
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(5.0),
-            borderSide: BorderSide(
-              color: CupertinoColors.black,
-              width: 1.0,
-            ),
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(5.0),
-            borderSide: BorderSide(
-              color: CupertinoColors.black,
-              width: 1.0,
-            ),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(5.0),
-            borderSide: BorderSide(
-              color: CupertinoColors.black,
-              width: 1.0,
-            ),
-          ),
-          contentPadding:
-              const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-        ),
+        decoration: textFieldDecoration,
         cursorColor: CupertinoColors.black,
         inputFormatters: formatters,
         textInputAction: TextInputAction.done,
